cfd4fd78b229cc4ac08e87c67a8b3f735d6f2128
adalbertus <wojciech@pietkiewicz.pl>
2020-06-20 23:45:36 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let sensorLeft = 0
+let sensorRight = 0
+
+pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+
 function driveForward(ms = 0) {
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 180)
@@ -7,7 +13,7 @@ function driveForward(ms = 0) {
     }
 }
 
-function driveBackward(ms = 0) {
+function driveBackward(ms = 100) {
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 180)
     if(ms > 0) {
@@ -38,6 +44,35 @@ function driveLeft(ms = 100) {
         driveStop()
     }
 }
+
+function turn180(ms = 100) {
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 180)
+    if(ms > 0) {
+        pause(ms)
+        driveStop()
+    }
+}
+
+function goSumo() {
+    sensorRight = pins.digitalReadPin(DigitalPin.P15)
+    sensorLeft = pins.digitalReadPin(DigitalPin.P16)
+
+    if (sensorRight == 1 && sensorLeft == 1) { // move forward
+    	driveForward() 
+    } else if (sensorRight == 1 && sensorLeft == 0) { // turn left
+    	driveBackward() 
+    } else if (sensorRight == 0 && sensorLeft == 1) { // turn right
+    	driveBackward()    	
+    } else { // stop
+        driveBackward()
+        turn180()        
+    }
+}
+
+input.onButtonPressed(Button.A, function () {
+    turn180()    
+})
 
 
 basic.forever(function () {
